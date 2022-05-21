4df4d3d14c6955996a4447b921e697004de7e475
alfazsozib <alfazsozib@gmail.com>
2022-05-21 15:59:40 +0600

    'numbering'

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Header />    
+      <Header />  
+      <Portfolio />  
       <Section />
-      <Portfolio />
       <Contact />
       <Footer />
       
